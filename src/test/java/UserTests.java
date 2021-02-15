efef96425ddf806b8d23cf687168e41b4396e9df
CGuthrieQA <CGuthrie@QA.com>
2021-02-15 16:27:38 +0000

    added acceptance criteria

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -24,24 +24,40 @@ public class UserTests {
     @Test
     public void test() {
     	System.out.println("starting test");
+    	
+    	// GIVEN - that I can access the demo site
     	System.out.println("navigating to site thedemosite.co.uk");
         driver.get("http://thedemosite.co.uk");
+        
+        // WHEN - I go to the add user page
         System.out.println("clicking the add a user link");
         driver.findElementByXPath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]").click();
+        
+        // AND - I enter my user credentials
         System.out.println("setting username to guest");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input").sendKeys("guest");
+        
         System.out.println("setting password to password");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input").sendKeys("password");
+        
         System.out.println("clicking the save button");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input").click();
+        
+        // AND - I go to the login page
         System.out.println("clicking the login link");
         driver.findElementByXPath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[4]").click();
+        
+        // AND - I enter my user credentials
         System.out.println("entering the username guest");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/p/input").sendKeys("guest");
+        
         System.out.println("entering the password password");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/p/input").sendKeys("password");
+        
         System.out.println("clicking the test login button");
         driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input").click();
+        
+        // THEN - the login is successful
         System.out.println("checking that the login was successful");
         assertEquals("**Successful Login**", driver.findElementByXPath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b").getText());
     }
